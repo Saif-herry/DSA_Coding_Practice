872a94b53d5da110ca6358bf42ac75631af5c61f
Mohammad Saif <saifsami321md@gmail.com>
2022-12-06 15:54:32 +0530

    two sum

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -1,4 +1,9 @@
+/*
+arr = [2,7,11,15]
+n = 4
+k = 9
 
+*/
 
 function TwoSum(num){
    let left = 0;
@@ -6,15 +11,19 @@ function TwoSum(num){
    let sum = 9;
    let flag = false;
    while(left<right){
+    // console.log(num[left]+num[right])
     if(sum == num[left]+num[right]){
           flag = true;
           break;
+        //   console.log("1")
     }
-    else if(sum>num[left]+num[right]){
+    if(sum<num[left]+num[right]){
         right--;
+        // console.log("2")
     }
-    else if(sum<num[left]+num[right]){
+    if(sum>num[left]+num[right]){
         left++;
+        // console.log("3",num[left])
     }
    }
    if(flag == true){
