ae967b62f0db3d2ee5f5861e314f12f5b394ac04
Mohammad Saif <saifsami321md@gmail.com>
2022-12-08 13:19:56 +0530

    missingInteger

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -1,36 +1,52 @@
 /*
-arr = [2,7,11,15]
-n = 4
-k = 9
-
+arr = [[1,3],[3,4],[4,6],[6,7]]
 */
 
 
-function TwoSum(n,k,arr){
-    let left = 0;
-    let right = n-1;
-    sum = 0;
-    let flag = false;
-    while(left<right){
-        sum = arr[left] + arr[right]
-        if(sum == k){
-            flag = true;
-            break;
-        }
-        if(sum>k){
-            right--;
-        }
-        if(sum<k){
-            left++;
-        }
-    }
-    if(flag==true){
-        console.log(left,right)
-    }
-    else{
-        console.log("-1","-1")
-    }
+// function Duplicate(arr){
+// let obj = {};
+// for(let i=0;i<arr.length;i++){
+//     let temp = arr[i]
 
+//     for(let j=0;j<temp.length;j++){
+//         if(obj[temp[j]]==undefined){
+//             obj[temp[j]]=1
+//         }
+//         else{
+//             obj[temp[j]]++
+//         }
+//     }
+// }
+// console.log(Object.keys(obj))
+// console.log(obj)
+// }
+
+// Duplicate([[1,3],[3,4],[4,6],[6,7]])
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Missing Integer 
+Sample Input--> 4 5 1 3
+Sample Output--> 2
+*/
+
+function MissingInteger(arr){
+
+   let n = arr.length+1;
+   let sum = (n*(n+1))/2
+
+   for(let i=0;i<arr.length;i++){
+    sum -= arr[i]
+   }
+   console.log(sum)
 }
 
-TwoSum(4,9,[2,7,11,15])
+MissingInteger([4,5,1,3])
+
+
+
+
+
+
+
