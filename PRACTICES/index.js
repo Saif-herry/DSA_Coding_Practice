ea46171d76afda005211951ca3c59768ac6f8cc7
Mohammad Saif <saifsami321md@gmail.com>
2022-12-09 11:19:15 +0530

    missing Integer

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -2,28 +2,6 @@
 arr = [[1,3],[3,4],[4,6],[6,7]]
 */
 
-
-// function Duplicate(arr){
-// let obj = {};
-// for(let i=0;i<arr.length;i++){
-//     let temp = arr[i]
-
-//     for(let j=0;j<temp.length;j++){
-//         if(obj[temp[j]]==undefined){
-//             obj[temp[j]]=1
-//         }
-//         else{
-//             obj[temp[j]]++
-//         }
-//     }
-// }
-// console.log(Object.keys(obj))
-// console.log(obj)
-// }
-
-// Duplicate([[1,3],[3,4],[4,6],[6,7]])
-
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 Missing Integer 
@@ -32,14 +10,14 @@ Sample Output--> 2
 */
 
 function MissingInteger(arr){
-
-   let n = arr.length+1;
-   let sum = (n*(n+1))/2
-
-   for(let i=0;i<arr.length;i++){
-    sum -= arr[i]
-   }
-   console.log(sum)
+      
+    let n = arr.length+1;
+    let output = 0;
+    sum_of_n_num = (n*(n+1))/2;
+    for(let i=0;i<arr.length;i++){
+        sum_of_n_num -=arr[i];
+    }
+    console.log(sum_of_n_num)
 }
 
 MissingInteger([4,5,1,3])
