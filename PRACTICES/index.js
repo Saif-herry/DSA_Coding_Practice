9fbddec6e2c04c3c6fa308f8cc8333725ed70156
Mohammad Saif <saifsami321md@gmail.com>
2022-12-10 20:05:25 +0530

    Armstrong number

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -4,59 +4,21 @@ arr = [[1,3],[3,4],[4,6],[6,7]]
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-Missing Integer 
-Sample Input--> 1 1 2 3 3
-Sample Output--> 5
-*/
-/*
-function DuplicateElement(arr){
-    let obj = {};
+Armstrong Number:
 
-    for(let i=0;i<arr.length;i++){
-        if(obj[arr[i]]==undefined){
-            obj[arr[i]] = 1;
-        }
-        else{
-            obj[arr[i]]++
-        }
-    }
-      
-    console.log(Object.keys(obj).join(" "))
-} 
+Sample input: 153
+sample output: 153
 
-DuplicateElement([1,1,2,3,3])
 */
 
+function ArmStrongNumber(num){
 
-/*
-power of 2
-
-sample input : 7
-sample output : false
-
-sample input : 8
-sample output : true
-*/
-
-function power_of_2(num){
-    
-let flag = false;
-while(num>1){
-    if(num%2==0){
-        num = num/2;
-        flag = true;
+    num = num.toString()
+    let output = 0
+    for(let i of num){
+        output += parseInt(i)**num.length
     }
-    else{
-        flag = false;
-        break;
-    }
+    output===parseInt(num)?console.log("ArmStrong"):console.log("Not ArmStrong")
 }
-flag==true?console.log("true"):console.log("false")
-}
-
-power_of_2(8)
-
-
-
-
+ArmStrongNumber(154)
 
