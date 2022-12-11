e132d6574ecb516d0fb17fae858f4e38cd469ba1
Mohammad Saif <saifsami321md@gmail.com>
2022-12-11 11:17:48 +0530

    moveZeroes question done

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -1,10 +1,26 @@
+
+
 /*
-arr = [[1,3],[3,4],[4,6],[6,7]]
+arr = [0,1,0,3,12]
 */
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-nums = [0]
-*/
+function MoveZeroes(arr){
+     
+    if(arr.length==1) return arr;
+    let j=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!=0){
+            arr[j]=arr[i];
+            j++;
+        }
+    }
+
+    for(let i=j;i<arr.length;i++){
+        arr[i] = 0
+    }
+    return arr;
+}
+
+console.log(MoveZeroes([0,1,0,3,12]))
 
 
