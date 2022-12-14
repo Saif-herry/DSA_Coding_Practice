980ae7fe23bb783f9e3fe3d21271f606428e4cca
Mohammad Saif <saifsami321md@gmail.com>
2022-12-14 23:50:26 +0530

    error occured in duplicate in 2d array

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -4,16 +4,18 @@ arr = [[1,3],[3,4],[4,6],[6,7]]
 
 
 function Duplicate(arr){
-     
-   let obj = {}
-   for(let i=0;i<arr.length;i++){
+    
+    let obj ={};
+    for(let i=0;i<arr.length;i++){
         let temp = arr[i];
-
         for(let j=0;j<temp.length;j++){
-            obj[temp[j]]=1
+            obj[temp[i]] = 1
         }
-   }
-   console.log(Object.keys(obj))
+
+    }
+    console.log(Object.keys(obj))
+
+
 }
 
 Duplicate([[1,3],[3,4],[4,6],[6,7]])
