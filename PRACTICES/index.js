3e104acc1f4ccee7504067f78e5a6a6c6f4445d0
Mohammad Saif <saifsami321md@gmail.com>
2022-12-14 23:59:46 +0530

    error occure

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -5,7 +5,19 @@ arr = [2,7,11,15]
 */
 
 function TwoSum(n,k,arr){
-
+  let left = 0;
+  let right = n-1;
+  let flag = false;
+  while(left<right){
+    let sum = arr[left]+arr[right]
+       if(sum==k){
+        flag = right
+        break
+       }
+       else if(sum>k){
+          
+       }
+  }
 }
 
 TwoSum(4,9,[2,7,11,15])
