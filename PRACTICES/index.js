d27d5f9c178765d1ff18926c69859e6ea28fc7d5
Mohammad Saif <saifsami321md@gmail.com>
2022-12-19 23:06:08 +0530

    Second Largest done

--- PRACTICES/index.js
+++ PRACTICES/index.js
@@ -5,15 +5,14 @@ arr = [2,7,4,9]
 
 const Second_largest=(arr)=>{
 
-    let F_Largest = -1, S_Largest = -1;
+    let F_LArgest = -1, S_Largest = -1
     for(let i=0;i<arr.length;i++){
-      if(arr[i]>F_Largest){
-       
-        S_Largest = F_Largest;
-        F_Largest = arr[i];
+      if(arr[i]>F_LArgest){
+        S_Largest = F_LArgest;
+        F_LArgest = arr[i];
       }
       else{
-        if(arr[i]>S_Largest && F_Largest!== arr[i]){
+        if(arr[i]>S_Largest && F_LArgest!== arr[i]){
           S_Largest = arr[i]
         }
       }
